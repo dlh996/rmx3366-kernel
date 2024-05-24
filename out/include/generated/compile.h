44531020980b1f34d3b1f13fc782b46d15ea4750
lr-v <du__th@163.com>
2024-05-24 13:45:16 +0800

    add

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Fri May 24 13:40:10 CST 2024"
+#define LINUX_COMPILE_BY "dlh"
+#define LINUX_COMPILE_HOST "DESKTOP-74HH7AR"
+#define LINUX_COMPILER "Proton clang version 13.0.0 (https://github.com/llvm/llvm-project b4fd512c36ca344a3ff69350219e8b0a67e9472a), GNU ld (GNU Binutils) 2.36.1"
